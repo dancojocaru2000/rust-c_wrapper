f8faf385686a596f422e8c8828e236928d831223
Dan Cojocaru <dan.cojocaru00@e-uvt.ro>
2020-12-17 17:18:25 +0200

    Implemented exec

---
+++ src/exec.rs
@@ -0,0 +1,108 @@
+use std::{ffi::CString, ptr::null_mut};
+
+use crate::{c_error::CError, c_result::CResult};
+
+pub unsafe fn exec(pathname: &str, argv: Vec<String>) -> CResult<()> {
+	let pathname = CString::new(pathname).unwrap();	// A Rust String will never error
+	let mut argv: Vec<_> = argv.into_iter().map(
+		|arg| CString::new(arg).unwrap()
+	).map(
+		|arg| arg.into_raw() as *const libc::c_char	// Transfer ownership to C
+	).collect();
+
+	argv.push(null_mut());
+
+	let argv_ptr = argv.as_ptr();
+
+	let result = libc::execv(pathname.as_ptr(), argv_ptr);
+
+	// At this point, the program continues only if execv encountered an error
+
+	assert!(result == -1);
+
+	Err(CError::new_from_errno())
+}
+
+pub unsafe fn exece(pathname: &str, argv: Vec<String>, env: Vec<String>) -> CResult<()> {
+	let pathname = CString::new(pathname).unwrap();	// A Rust String will never error
+	let mut argv: Vec<_> = argv.into_iter().map(
+		|arg| CString::new(arg).unwrap()
+	).map(
+		|arg| arg.into_raw() as *const libc::c_char	// Transfer ownership to C
+	).collect();
+
+	argv.push(null_mut());
+
+	let argv_ptr = argv.as_ptr();
+
+	let mut env: Vec<_> = env.into_iter().map(
+		|arg| CString::new(arg).unwrap()
+	).map(
+		|arg| arg.into_raw() as *const libc::c_char	// Transfer ownership to C
+	).collect();
+
+	env.push(null_mut());
+
+	let env_ptr = env.as_ptr();
+
+	let result = libc::execve(pathname.as_ptr(), argv_ptr, env_ptr);
+
+	// At this point, the program continues only if execv encountered an error
+
+	assert!(result == -1);
+
+	Err(CError::new_from_errno())
+}
+
+pub unsafe fn execp(file: &str, argv: Vec<String>) -> CResult<()> {
+	let file = CString::new(file).unwrap();	// A Rust String will never error
+	let mut argv: Vec<_> = argv.into_iter().map(
+		|arg| CString::new(arg).unwrap()
+	).map(
+		|arg| arg.into_raw() as *const libc::c_char	// Transfer ownership to C
+	).collect();
+
+	argv.push(null_mut());
+
+	let argv_ptr = argv.as_ptr();
+
+	let result = libc::execvp(file.as_ptr(), argv_ptr);
+
+	// At this point, the program continues only if execv encountered an error
+
+	assert!(result == -1);
+
+	Err(CError::new_from_errno())
+}
+
+pub unsafe fn execpe(file: &str, argv: Vec<String>, env: Vec<String>) -> CResult<()> {
+	let file = CString::new(file).unwrap();	// A Rust String will never error
+	let mut argv: Vec<_> = argv.into_iter().map(
+		|arg| CString::new(arg).unwrap()
+	).map(
+		|arg| arg.into_raw() as *const libc::c_char	// Transfer ownership to C
+	).collect();
+
+	argv.push(null_mut());
+
+	let argv_ptr = argv.as_ptr();
+
+	let mut env: Vec<_> = env.into_iter().map(
+		|arg| CString::new(arg).unwrap()
+	).map(
+		|arg| arg.into_raw() as *const libc::c_char	// Transfer ownership to C
+	).collect();
+
+	env.push(null_mut());
+
+	let env_ptr = env.as_ptr();
+
+	let result = libc::execvpe(file.as_ptr(), argv_ptr, env_ptr);
+
+	// At this point, the program continues only if execv encountered an error
+
+	assert!(result == -1);
+
+	Err(CError::new_from_errno())
+}
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod fork;
 pub mod c_error;
 pub mod c_result;
+pub mod exec;
 pub mod wait;
